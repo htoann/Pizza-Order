6209e356b629ddddc021b3cc8537addc37cde95b
HToan <huutrantoan@gmail.com>
2021-07-25 23:23:05 +0700

    Webpack mix

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ const path = require("path");
 const expressLayout = require("express-ejs-layouts");
 const PORT = process.env.PORT || 3000;
 
+// Assets
+app.use(express.static("public"));
+
 app.get("/", (req, res) => {
   res.render("home");
 });
---
+++ webpack.mix.js
@@ -0,0 +1,7 @@
+// webpack.mix.js
+
+let mix = require("laravel-mix");
+
+mix
+  .js("resources/js/app.js", "public/js/app.js")
+  .sass("resources/scss/app.scss", "public/css/app.css");
