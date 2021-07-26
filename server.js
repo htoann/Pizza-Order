8ed159043123d5b4c89cc990736008df0bf3f5ac
HToan <huutrantoan@gmail.com>
2021-07-26 21:36:28 +0700

    add cart, auth page

--- server.js
+++ server.js
@@ -8,14 +8,26 @@ const PORT = process.env.PORT || 3000;
 // Assets
 app.use(express.static("public"));
 
-app.get("/", (req, res) => {
-  res.render("home");
-});
-
 // set Template engine
 app.use(expressLayout);
 app.set("views", path.join(__dirname, "/resources/views"));
 app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+app.get("/cart", (req, res) => {
+  res.render("customers/cart");
+});
+
+app.get("/login", (req, res) => {
+  res.render("auth/login");
+});
+
+app.get("/register", (req, res) => {
+  res.render("auth/register");
+});
 
 app.listen(PORT, () => {
   console.log(`Listening on port http://localhost:${PORT}`);
