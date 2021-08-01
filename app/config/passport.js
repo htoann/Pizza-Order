a2d437b4130dd83b4e46387d6f70f5e047cbe206
HToan <huutrantoan@gmail.com>
2021-08-01 12:21:46 +0700

    Make it better

--- app/config/passport.js
+++ app/config/passport.js
@@ -1,5 +1,5 @@
 const LocalStrategy = require("passport-local").Strategy;
-const User = require("../models/User");
+const User = require("../models/user");
 const bcrypt = require("bcrypt");
 
 module.exports = function init(passport) {
