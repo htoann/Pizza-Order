2326c55f6a05c01a2e349524f639e0a62cef17fe
HToan <huutrantoan@gmail.com>
2021-08-01 12:29:31 +0700

    Make it better

--- app/http/controllers/authController.js
+++ app/http/controllers/authController.js
@@ -1,4 +1,4 @@
-const User = require("../../models/User");
+const User = require("../../models/user");
 const bcrypt = require("bcrypt");
 const saltRounds = 10;
 const passport = require("passport");
