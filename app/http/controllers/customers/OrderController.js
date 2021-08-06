193bdd4fbf3695c365f592bf32793fbb3cd2d818
HToan <huutrantoan@gmail.com>
2021-08-06 18:20:10 +0700

    Deploy to Heroku

--- app/http/controllers/customers/OrderController.js
+++ app/http/controllers/customers/OrderController.js
@@ -1,4 +1,4 @@
-const Order = require("../../../models/Order");
+const Order = require("../../../models/order");
 const moment = require("moment");
 
 class OrderController {
--- routes/authRouter.js
+++ routes/authRouter.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const guest = require("../app/http/middleware/Guest");
+const guest = require("../app/http/middleware/guest");
 const authController = require("../app/http/controllers/AuthController");
 
 router.get("/login", guest, authController.login);
