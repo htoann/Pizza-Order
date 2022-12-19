3c57e0b65fae72811b221e138e63107ceab95a48
Tran Toan <huutrantoan@gmail.com>
2022-12-19 21:56:19 +0700

    build: fix deploy

--- server.js
+++ server.js
@@ -20,7 +20,6 @@ mongoose
   .connect(`${process.env.MONGODB_URI}`, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    useCreateIndex: true,
   })
   .catch((err) => console.log(err));
 
